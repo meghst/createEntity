ba4c909514bb3b8ade858f4b09e20516ec813416
meghst <meghana.thiyyakat@flipkart.com>
2015-05-16 16:48:25 +0530

    Populate foreign key options based on table

--- js/controllers/create-controller.js
+++ js/controllers/create-controller.js
@@ -2,11 +2,13 @@ angular.module('mdmUI.controllers')
 .controller('create',
     function($scope, $http) {
 
-    	$scope.colNames=[]
-    	$scope.relationships=[]
-    	$scope.indexes=[]
-    	$scope.tableNames=["A","B"]
     	$scope.fkId=1;
     	$scope.indexId=1;
     	$scope.columnId=1;
+    	
+    	$scope.tableNames=["A","B"]
+    	$scope.colNames={"A":["a","b"],"B":["c","d"]}
+    	
+    	
+    	
 });
--- js/directives/create-directive.js
+++ js/directives/create-directive.js
@@ -6,6 +6,28 @@ angular.module('mdmUI.directives',[])
     directive.templateUrl = "views/create-entity.html";
     directive.link = function(scope, element, attrs) {
 
+        scope.addField = function () {
+            var el = $compile( "<field></field>" )( scope );
+            el.attr("id","col"+scope.columnId)
+            element.find("field").last().append( el );
+            scope.columnId++;
+        };
+
+        scope.addForeignKey = function () {
+
+            var el = $compile( "<foreign-key></foreign-key>" )( scope );
+            var temp=el.attr("id","fk"+scope.fkId)
+            element.find("foreign-key").last().append( el );
+            scope.fkId++;
+        };
+
+        scope.addEntityIndex = function () {
+            var el = $compile( "<entity-index><entity-index>" )( scope );
+            el.attr("id","index"+scope.indexId)
+            element.find("entity-index").last().append( el );
+            scope.indexId++;
+        };
+
 
     };
     return directive;
@@ -16,24 +38,11 @@ angular.module('mdmUI.directives',[])
     var directive = {};
     directive.restrict = 'E';
     directive.templateUrl = "views/field.html";
-    directive.link = function(scope, element, attrs) {
-
-	    scope.addField = function () {
-	        var el = $compile( "<field></field>" )( scope );
-            el.attr("id","col"+scope.columnId)
-            console.log(el.find("[name=colName]").bind("change",function()
-            {   
-                $("[name=colName]").each(function(){
-                    scope.colNames.push($(this).value())
-                })
-                console.log(scope.colNames)
-            }));
-	        element.parent().append( el );
-            scope.columnId++;
-	    };
+    directive.link = function(scope, element, attrs) {  
     };
 
     return directive;
+
 })
 
 .directive("foreignKey",function($compile) {
@@ -41,12 +50,22 @@ angular.module('mdmUI.directives',[])
     directive.restrict = 'E';
     directive.templateUrl = "views/foreign-key.html";
     directive.link = function(scope, element, attrs) {
-        scope.addForeignKey = function () {
-	        var el = $compile( "<foreign-key></foreign-key>" )( scope );
-            el.attr("id","fk"+scope.fkId)
-            element.parent().append( el );
-            scope.fkId++;
-            };
+        
+        element.find("[name=ftable]").change(function(){
+            var table=$(this).val()
+            console.log(table)
+            if(table=="")
+            {
+                console.log("heres")
+                element.find("[name=fkey]").val("").attr("disabled","disabled")
+                return;
+            }
+            var fkey=element.find("[name=fkey]").attr("disabled",false)
+            fkey.empty()
+            $.each(scope.colNames[table],function(key,value){
+                fkey.append("<option value="+value+">"+value+"</option>")
+            })
+        })    
     };
 
     return directive;
@@ -57,13 +76,6 @@ angular.module('mdmUI.directives',[])
     directive.restrict = 'E';
     directive.templateUrl = "views/entity-index.html";
     directive.link = function(scope, element, attrs) {
-
-	    scope.addEntityIndex = function () {
-	        var el = $compile( "<entity-index><entity-index>" )( scope );
-            el.attr("id","index"+scope.indexId)
-	        element.parent().append( el );
-            scope.indexId++;
-	    };
     };
 
     return directive;
